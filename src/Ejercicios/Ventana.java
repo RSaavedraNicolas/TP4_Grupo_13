d7a023521781501a73ede7894ee2150440735d94
unknown <palmierifacundo@gmail.com>
2024-05-01 19:08:17 -0300

    Se agregan 3 botones y nombre a la ventana

--- src/Ejercicios/Ventana.java
+++ src/Ejercicios/Ventana.java
@@ -1,15 +1,45 @@
 package Ejercicios;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 public class Ventana extends JFrame{
+	
+	private JButton boton;
+	private JTextField txtNombre;
+	
 
 	public Ventana() {
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(500,500);
+		setSize(450,300);
+		
 		setLocation(350,250);
-		setTitle("Menu Principal");
+		setTitle("GRUPO NRO: 13");
+		setLayout(null);
+		
+		
+		//Creación de botón Ejercicio 1
+		boton = new JButton();
+		boton.setText("Ejercicio 1");
+		boton.setBounds(140, 50, 140, 30);
+		getContentPane().add(boton);
+		
+		
+		//Creación de botón Ejercicio 2
+		boton = new JButton();
+		boton.setText("Ejercicio 2");
+		boton.setBounds(140, 100, 140, 30);
+		getContentPane().add(boton);
+		
+		//Creación de botón Ejercicio 3
+		boton = new JButton();
+		boton.setText("Ejercicio 3");
+		boton.setBounds(140, 150, 140, 30);
+		getContentPane().add(boton);
+		
+		
 		
 	}
 
